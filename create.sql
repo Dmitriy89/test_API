35c1164ece64eddd299b4232c0acbd3855469d80
Dmitriy89 <dmitriy-blinkov@bk.ru>
2019-03-03 00:32:59 +0300

    Delete create.sql

--- create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `testing` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`reqjson` LONGTEXT NOT NULL DEFAULT '' COLLATE 'utf8mb4_bin',
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
